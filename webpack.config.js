280a51e53eb6426361848ac71d4519ade042488b
ManikandanSriramDev <sriramsha67@gmail.com>
2024-12-21 10:38:37 +0530

    Initial Update

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-export const module = {
-    rules: [
-        {
-            test: /\.html$/, // Match all HTML files
-            use: ['html-loader'], // Use html-loader to process them
-        },
-    ],
-};
-  
